bc7b9f5045e71e5c51c470b1927b128341a4e53a
Fadhil <fadhilahdwi.a@gmail.com>
2024-05-12 00:54:03 +0700

    update view data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,11 +8,63 @@ Route::get('/', function () {
 });
 
 Route::get('/about', function () {
-    return view('about', ['title' => 'Blog', 'name' => 'Fadhil Dwi A']);
+    return view('about', ['title' => 'About', 'name' => 'Fadhil Dwi A']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Fadhil Dwi A',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa omnis quam totam
+            provident accusamus
+            voluptatum aspernatur odio ea repellendus amet numquam iure saepe similique vel accusantium quod non, magni
+            animi!'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Fadhil Dwi A',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa omnis quam totam
+            provident accusamus
+            voluptatum aspernatur odio ea repellendus amet numquam iure saepe similique vel accusantium quod non, magni
+            animi!'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Fadhil Dwi A',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa omnis quam totam
+            provident accusamus
+            voluptatum aspernatur odio ea repellendus amet numquam iure saepe similique vel accusantium quod non, magni
+            animi!'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Fadhil Dwi A',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa omnis quam totam
+            provident accusamus
+            voluptatum aspernatur odio ea repellendus amet numquam iure saepe similique vel accusantium quod non, magni
+            animi!'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
