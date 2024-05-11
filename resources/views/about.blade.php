260ad7aadfa7d408674826e38f7d980f1a1c1651
Fadhil <fadhilahdwi.a@gmail.com>
2024-05-12 00:26:37 +0700

    init

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3>Owner: {{ $name }}</h3>
+    <h1>Halaman About</h1>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h1>Halaman Contact</h1>
+    <ul>
+        <li>
+            <a href="https://instagram.com">Instagram</a>
+        </li>
+        <li>
+            <a href="https://youtube.com">Youtube</a>
+        </li>
+    </ul>
+</x-layout>
